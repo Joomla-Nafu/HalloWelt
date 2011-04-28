7b8cea1e4cf55e77ed12d39929dd8efdb0bbe6da
elkuku <elkuku>
2011-04-28 16:18:50 +0000

    -x

--- hallowelt_1.6/sources/2/site/controller.php
+++ hallowelt_1.6/sources/2/site/controller.php
@@ -3,7 +3,7 @@
 defined('_JEXEC') or die;
 
 // Die Joomla! Controllerbibliothek importieren
-jimport('joomla.application.component.controllerx');
+jimport('joomla.application.component.controller');
 
 // Die Klasse erweitert die Klasse JController
 class HalloWeltController extends JController
