7ae29313e5f14d64f42e364c842a5e499e541836
Nikolai Plath <der.el.kuku@gmail.com>
2012-12-11 17:39:25 -0500

    Change a variable name

--- 3.0_legacy/sources/5/site/models/hallowelt.php
+++ 3.0_legacy/sources/5/site/models/hallowelt.php
@@ -1,45 +1,43 @@
 <?php
-// Den direkten Aufruf verbieten
+// Den direkten Aufruf verbieten.
 defined('_JEXEC') or die;
- 
-// Die Joomla! Modelitem Klasse importieren
-jimport('joomla.application.component.modelitem');
- 
-// Diese Klasse erweitert die JModelitem Klasse
+
+// Diese Klasse erweitert die JModelitem Klasse.
 class HalloWeltModelHalloWelt extends JModelItem
 {
     /**
-     * @var string msg
+     * @var string
      */
-    protected $msg;
- 
-    /**
-     * Get the message.
-     * @return string The message to be displayed to the user
-     */
-    public function getMsg()
+    protected $hallo = '';
+
+	/**
+	 * Gibt ein "Hallo" zurück.
+	 *
+	 * @return string
+	 */
+    public function getHallo()
     {
-        if ( ! isset($this->msg))
+        if ('' == $this->hallo)
         {
-            $id = JRequest::getInt('id');
- 
+            $id = JFactory::getApplication()->input->getInt('id');
+
             switch ($id)
             {
                 case 1:
-                    $this->msg = JText::_('COM_HALLOWELT_OPTION_1');
+                    $this->hallo = JText::_('COM_HALLOWELT_OPTION_1');
                     break;
- 
+
                 case 2:
-                    $this->msg = JText::_('COM_HALLOWELT_OPTION_2');
+                    $this->hallo = JText::_('COM_HALLOWELT_OPTION_2');
                     break;
- 
+
                 default:
-                    $this->msg = JText::_('COM_HALLOWELT_UNDEFINED_MESSAGE');
- 
+                    $this->hallo = JText::_('COM_HALLOWELT_UNDEFINED_MESSAGE');
+
                     break;
             }
         }
- 
-        return $this->msg;
+
+        return $this->hallo;
     }
 }
