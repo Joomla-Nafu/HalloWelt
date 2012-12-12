cfe2c18da7554970a846e59e4a195023b2c33878
Nikolai Plath <der.el.kuku@gmail.com>
2012-12-11 19:15:43 -0500

    Update #14

--- 3.0_legacy/sources/14/admin/hallowelt.php
+++ 3.0_legacy/sources/14/admin/hallowelt.php
@@ -8,14 +8,11 @@ if ( ! JFactory::getUser()->authorise('core.manage', 'com_hallowelt'))
     return JError::raiseWarning(404, JText::_('JERROR_ALERTNOAUTHOR'));
 }
 
-// Die Joomla! Controllerbibliothek importieren
-jimport('joomla.application.component.controller');
-
 // Die Helperdatei registrieren
 JLoader::register('HalloWeltHelper', JPATH_COMPONENT.'/helpers/hallowelt.php');
 
 // Eine Instanz des Controllers mit dem Präfix 'HalloWelt' beziehen
-$controller = JController::getInstance('HalloWelt');
+$controller = JControllerLegacy::getInstance('HalloWelt');
 
 // Den 'task' der im Request übergeben wurde ausführen
 $controller->execute(JRequest::getCmd('task'));
